1f18d5250d2eb342693a20f3f95ce5f4a222f1f8
jguzmanps4 <jguzmanps4@gmail.com>
2024-10-28 20:24:41 -0400

    added add/remove button

---
+++ script.js
@@ -0,0 +1,41 @@
+let todoList = [
+    "Demo Task 1",
+    "Demo Task 2"
+]
+
+const listElement = document.getElementById("list")
+const addButton = document.getElementById("addbtn")
+const deleteButton = document.getElementById("deletebtn")
+
+
+function updateTasks() {
+    listElement.innerHTML = "";
+    for (let i = 0; i < todoList.length; i++){
+         let newTaskElement = document.createElement("li");
+        newTaskElement.textContent = todoList[i];
+        listElement.appendChild(newTaskElement);
+    }
+}
+
+function addTask(taskName) {
+    todoList.push(taskName);
+    updateTasks();
+}
+
+function deleteTask() {
+    todoList.pop();
+    updateTasks();
+}
+
+addButton.addEventListener("click", () => {
+    const task = prompt("Enter task name")
+    addTask(task)
+})
+
+deleteButton.addEventListener("click", () => {
+    deleteTask()
+})
+
+updateTasks()
+
+console.log(listElement)
